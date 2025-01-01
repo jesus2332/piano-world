d72e5540572ca92a445f56a6e93eb505ef62c551
ManuelCalleros_ <33211111+jesus2332@users.noreply.github.com>
2024-12-31 21:55:53 -0600

    Parte del proyecto estatico
    Añadida la parte estatica del proyecto de pianoworld

---
+++ README.md
@@ -0,0 +1,57 @@
+
+# Piano World
+
+Piano World is a web application built with React and Vite that showcases a variety of keyboards.
+
+## Features
+
+- Display a collection of pianos with images, descriptions, and prices.
+- Add pianos to the shopping cart.
+
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/piano-world.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd piano-world
+   ```
+
+3. Install the dependencies:
+
+   ```bash
+   npm install
+   ```
+
+## Usage
+
+To start the development server, run:
+
+```bash
+npm run dev
+```
+
+This will launch the application and you can view it in your browser at `http://localhost:3000`.
+
+## Build
+
+To build the project for production, run:
+
+```bash
+npm run build
+```
+
+The output will be located in the `dist` directory.
+
+
+
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import {useState} from 'react'
+import Keyboard from "./components/Keyboard"
+import Header from "./components/Header"
+import { db } from './data/db'
+function App() {
+  const [data, setData] = useState(db)
+
+
+  
+  return (
+    <>
+    <Header />
+    <header className="py-5 header">
+        <div className="container-xl">
+            <div className="row justify-content-center justify-content-md-between">
+                <div className="col-8 col-md-3">
+                    <a href="index.html">
+                        <img className="img-fluid" src="./public/img/piano_logo.png" alt="imagen logo" />
+                    </a>
+                </div>
+                <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                    <div 
+                        className="carrito"
+                    >
+                        <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+
+                        <div id="carrito" className="bg-white p-3">
+                            <p className="text-center">El carrito esta vacio</p>
+                            <table className="w-100 table">
+                                <thead>
+                                    <tr>
+                                        <th>Imagen</th>
+                                        <th>Nombre</th>
+                                        <th>Precio</th>
+                                        <th>Cantidad</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <img className="img-fluid" src="./public/img/piano_02.jpg" alt="imagen piano" />
+                                        </td>
+                                        <td>SRV</td>
+                                        <td className="fw-bold">
+                                                $299
+                                        </td>
+                                        <td className="flex align-items-start gap-4">
+                                            <button
+                                                type="button"
+                                                className="btn btn-dark"
+                                            >
+                                                -
+                                            </button>
+                                                1
+                                            <button
+                                                type="button"
+                                                className="btn btn-dark"
+                                            >
+                                                +
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button
+                                                className="btn btn-danger"
+                                                type="button"
+                                            >
+                                                X
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
+                            <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((keyboard) =>{
+            return (
+              <Keyboard
+                key={keyboard.id}
+                keyboard={keyboard}
+              
+              />
+
+            )
+          })}
+           
+
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">Pianoworld - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+
+    </>
+  )
+}
+export default App
